202914238eb2e71e8a226ed6a16baace4886229d
WebDeveloper <74360914+Akimgan@users.noreply.github.com>
2021-03-12 15:31:12 +0300

    Add files via upload

---
+++ games/MyGame/game.js
@@ -0,0 +1,22 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+
+var player = new Image;
+var bg = new Image;
+var pipe = new Image;
+
+
+player.src = "img/player.зтп";
+bg.src = "img/bg.jpg"
+pipe.src = "img/pipe."
+
+
+	ctx.drawImage(bg, 0, 0);
+
+
+
+
+
+
+requestAnimationFrame(draw);
